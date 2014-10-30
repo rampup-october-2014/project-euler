5e8e7acd6f8c7cb802cfe1916ceabe27382aa259
cait porte <caitporte@gmail.com>
2014-10-30 19:43:06 -0400

    cait porte

---
+++ practice.rb
@@ -0,0 +1,29 @@
+def ethan_can_drink(name, age)
+if name = "Ethan"
+  if age >= 18
+  	puts "Ethan can drink"
+  else
+  	puts "Ethan cannot drink"
+  end
+ end
+
+if age >= 18
+  puts ""
+else
+  puts "#{name} cannot drink"
+end 
+
+
+unless name == "Ethan"
+	puts "okay"
+else
+	puts "hello"
+end
+
+if name != "Ethan"
+	puts "okay"
+end
+
+puts "Ethan" if name == "Ethan"
+
+
